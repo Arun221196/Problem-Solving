0d4c06517c76cce7febefcd4150599e07d61a62e
Arun221196 <arunkumar222951@gmail.com>
2025-12-23 21:39:42 +0530

    SSL insert better code(optimized)

--- Udemy/DSA/SinglyLinkedList/SLL - insert.js
+++ Udemy/DSA/SinglyLinkedList/SLL - insert.js
@@ -148,6 +148,19 @@ class SinglyLinkedList {
         }
         
     }
+    insert(index, val){ // better code(optimized)
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+        
+        var newNode = new Node(val);
+        var prev = this.get(index - 1);
+        var temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
 }
 let singlyLinkedList  = new SinglyLinkedList();
 // list.push('Hello')
