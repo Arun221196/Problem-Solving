32cadec5bf2e9326079688a3a189ad5d08fa71c9
Arun221196 <arunkumar222951@gmail.com>
2026-01-13 19:48:47 +0530

    search in linkedlist

---
+++ GeeksforGeeks/DSA/LinkedList/LL - Search in Linked List.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Node} head
+ * @param {number} key
+ * @returns {boolean}
+ */
+
+/**
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    searchKey(head, key) {
+        // Code here
+        while(head){
+            if(head.data === key){
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
