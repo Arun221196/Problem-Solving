5f18671d19fe5ce46d24cc558a92983ce5b03472
Arun221196 <arunkumar222951@gmail.com>
2026-02-18 20:22:33 +0530

    Get Min from Stack optimal solution

--- GeeksforGeeks/DSA/Stack/Get Min from Stack.js
+++ GeeksforGeeks/DSA/Stack/Get Min from Stack.js
@@ -2,7 +2,10 @@
  * Get Min from Stack
 https://www.geeksforgeeks.org/problems/get-minimum-element-from-stack/1?page=1&category=Stack&sortBy=submissions
  */
-
+/**
+ * time complexity: O(1) for all operations
+ * space complexity: O(n) in worst case when all elements are in decreasing order
+ */
 class SpecialStack {
     constructor() {
         // Define Stack
@@ -16,18 +19,18 @@ class SpecialStack {
         this.size++;
         this.stack[this.size] = x;
         let minStackLen = this.minStack.length;
-        if(minStackLen === 0 || this.minStack[minStackLen - 1] >= x){
+        if (minStackLen === 0 || this.minStack[minStackLen - 1] >= x) {
             this.minStack.push(x);
         }
     }
 
     pop() {
         // Pop the top element from the stack
-        if(this.size !== -1) {
+        if (this.size !== -1) {
             let popedEl = this.stack.pop();
             this.size--;
             let minStackLen = this.minStack.length;
-            if(this.minStack[minStackLen - 1] === popedEl){
+            if (this.minStack[minStackLen - 1] === popedEl) {
                 this.minStack.pop();
             }
         }
@@ -35,7 +38,7 @@ class SpecialStack {
 
     peek() {
         // Return the top element of the stack
-        if(this.size === -1){
+        if (this.size === -1) {
             return -1;
         }
         return this.stack[this.size];
@@ -50,5 +53,68 @@ class SpecialStack {
         // Get the minimum element in the stack
         let minStackLen = this.minStack.length;
         return minStackLen === 0 ? -1 : this.minStack[minStackLen - 1];
+    }
+}
+
+/**
+ * time complexity: O(1) for all operations
+ * space complexity: O(1) as we are using only one variable to store the minimum element
+ */
+
+class SpecialStack {
+    constructor() {
+        // Define Stack
+        this.stack = [];
+        this.min = Infinity;
+    }
+
+    push(x) {
+        // Push element onto the stack
+        if (this.stack.length === 0) {
+            this.stack.push(x);
+            this.min = x;
+        }
+        else {
+            if (x < this.min) {
+                let modVal = 2 * x - this.min;
+                this.stack.push(modVal);
+                this.min = x;
+            }
+            else {
+                this.stack.push(x);
+            }
+        }
+    }
+
+    pop() {
+        // Pop the top element from the stack
+        if (this.stack.length !== 0) {
+            let popedVal = this.stack.pop();
+            if (popedVal < this.min) {
+                this.min = this.min * 2 - popedVal;
+            }
+        }
+    }
+
+    peek() {
+        // Return the top element of the stack
+        if (this.stack.length === 0) {
+            return -1;
+        }
+        let topElement = this.stack[this.stack.length - 1];
+        return this.min < topElement ? topElement : this.min;
+    }
+
+    isEmpty() {
+        // Check if the stack is empty
+        return this.stack.length === 0;
+    }
+
+    getMin() {
+        // Get the minimum element in the stack
+        if (this.stack.length === 0) {
+            return -1;
+        }
+        return this.min;
     }
 }
